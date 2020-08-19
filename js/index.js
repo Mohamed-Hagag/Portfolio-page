b07be0d2efde2e207b7cd3813b3d8fc9f6ce342f
unknown <mo.hagag27@gmail.com>
2020-08-19 03:58:16 +0200

    second commitreset

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  let aboutOffset = $("#about").offset().top;
+  $(window).scroll(() => {
+    let wScroll = $(window).scrollTop();
+
+    if (wScroll > aboutOffset - 40) {
+      $("nav").addClass("bg-black");
+      $("#btn-up").fadeIn(200);
+    } else {
+      $("nav").removeClass("bg-black");
+      $("#btn-up").fadeOut(200);
+    }
+  });
+
+  $("#btn-up").click(() => {
+    $("html,body").animate({ scrollTop: "0" }, 1000);
+  });
+
+  $("a[href^='#']").click(function () {
+    let aHref = $(this).attr("href");
+    $("html,body").animate({ scrollTop: $(aHref).offset().top }, 1000);
+  });
+
+  $(".loading").fadeOut(1000, () => {
+    $("body").css("overflow", "auto");
+  });
+});
